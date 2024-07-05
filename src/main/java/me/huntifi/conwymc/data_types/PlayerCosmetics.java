9a4209b64826aab21eba133f180d46c52c6d6ac2
Greenfoot5 <greenfoot89@gmail.com>
2024-07-05 16:56:18 +0100

    Made isTop public

--- src/main/java/me/huntifi/conwymc/data_types/PlayerCosmetics.java
+++ src/main/java/me/huntifi/conwymc/data_types/PlayerCosmetics.java
@@ -148,6 +148,8 @@ public class PlayerCosmetics {
             Tuple<PreparedStatement, ResultSet> allTimeBoosters = LoadData.getAllTimeBoosters(0);
             isTop(data, uuid, allTimeBoosters.getSecond(), 3, benevolent);
             allTimeBoosters.getFirst().close();
+
+            Cosmetic reaper = new Cosmetic(TITLE, "Reaper", "<color:#3B3B3B>☠<b><gradient:#592E31:#92191E>Reaper</b>☠");
         } catch (SQLException e) {
             ConwyMC.plugin.getLogger().severe("Error in checkTopCosmetics for " + uuid);
             ConwyMC.plugin.getLogger().severe(e.getMessage());
@@ -166,7 +168,7 @@ public class PlayerCosmetics {
         }
     }
 
-    private static void isTop(PlayerData data, UUID uuid, ResultSet results, int maximumPosition, Cosmetic cosmetic) {
+    public static void isTop(PlayerData data, UUID uuid, ResultSet results, int maximumPosition, Cosmetic cosmetic) {
         try {
             for (int i = 0; i < maximumPosition; i++) {
                 results.next();
